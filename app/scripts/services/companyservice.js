f79acb6f304f7e972aba7a50b76931c0eea85676
Marmidon <nstenko1991@gmail.com>
2016-02-22 18:34:07 -0500

    Stock Adding part 1

--- app/scripts/services/companyservice.js
+++ app/scripts/services/companyservice.js
@@ -1,13 +1,5 @@
 'use strict';
-
-/**
- * @ngdoc service
- * @name stockWatchApp.CompanyService
- * @description
- * # CompanyService
- * Service in the stockWatchApp.
- */
 angular.module('stockWatchApp')
-  .service('CompanyService', function CompanyService() {
-    // AngularJS will instantiate a singleton by calling "new" on this function
-  });
+.service('CompanyService', function CompanyService($resource) {
+return $resource('companies.json');
+});
--- app/scripts/services/watchlist-service.js
+++ app/scripts/services/watchlist-service.js
@@ -1,6 +1,48 @@
 'use strict';
 angular.module('stockWatchApp')
 .service('WatchlistService', function WatchlistService() {
+    // Augment Stocks with additional helper functions
+var StockModel = {
+save: function () {
+var watchlist = findById(this.listId);
+watchlist.recalculate();
+saveModel();
+}
+};
+    // Augment watchlists with additional helper functions
+var WatchlistModel = {
+addStock: function (stock) {
+var existingStock = _.find(this.stocks, function (s) {
+return s.company.symbol === stock.company.symbol;
+});
+if (existingStock) {
+existingStock.shares += stock.shares;
+} else {
+_.extend(stock, StockModel);
+this.stocks.push(stock);
+}
+this.recalculate();
+saveModel();
+},
+    removeStock: function (stock) {
+_.remove(this.stocks, function (s) {
+return s.company.symbol === stock.company.symbol;
+});
+this.recalculate();
+saveModel();
+},
+recalculate: function () {
+var calcs = _.reduce(this.stocks, function (calcs, stock) {
+calcs.shares += stock.shares;
+calcs.marketValue += stock.marketValue;
+calcs.dayChange += stock.dayChange;   
+    return calcs;
+}, { shares: 0, marketValue: 0, dayChange: 0 });
+this.shares = calcs.shares;
+this.marketValue = calcs.marketValue;
+this.dayChange = calcs.dayChange;
+}
+};
 // [1] Helper: Load watchlists from localStorage
 var loadModel = function () {
 var model = {
